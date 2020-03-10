5172ff8e19d29aa58074d60ef7ce4bd41d7bf8c3
Constantine Ter-Matevosian <terkot1010@gmail.com>
2020-03-11 00:17:48 +0300

    Added a PropValue data constructor to the PropFormula type

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,6 +11,7 @@ import Data.Maybe
 
 data PropFormula
     = PropVariable    Char
+    | PropValue       LogicValue
     | Not             PropFormula
     | PropFormula :&  PropFormula
     | PropFormula :|  PropFormula
@@ -18,6 +19,7 @@ data PropFormula
 
 instance Show PropFormula where
     show (PropVariable x) = [x]
+    show (PropValue    v) = if v == T then "1" else "0"
     show (Not          p) = "!(" ++ show p  ++ ")"
     show (p1    :&    p2) =  "(" ++ show p1 ++ " & "  ++ show p2 ++ ")"
     show (p1    :|    p2) =  "(" ++ show p1 ++ " v "  ++ show p2 ++ ")"
@@ -26,27 +28,6 @@ instance Show PropFormula where
 infixr 1 :->
 infixr 2 :|
 infixr 3 :&
-
-extractVariables :: PropFormula -> [Char]
-extractVariables p = removeDuplicates $ extractVariables' p []
-
-extractVariables' :: PropFormula -> [Char] -> [Char]
-extractVariables' (PropVariable ch) acc =   ch : acc
-extractVariables' (Not           p) acc =   extractVariables' p  acc
-extractVariables' (p1    :&     p2) acc =  (extractVariables' p1 acc) 
-                                        ++ (extractVariables' p2 acc)
-                                        ++  acc
-extractVariables' (p1    :|     p2) acc =  (extractVariables' p1 acc) 
-                                        ++ (extractVariables' p2 acc)
-                                        ++  acc
-extractVariables' (p1    :->    p2) acc =  (extractVariables' p1 acc) 
-                                        ++ (extractVariables' p2 acc)
-                                        ++  acc
-
-removeDuplicates :: Eq a => [a] -> [a]
-removeDuplicates = foldl (\seen x -> if x `elem` seen
-                                     then seen
-                                     else seen ++ [x]) []
 
 data LogicValue = L | T deriving (Show, Eq)
 
@@ -70,6 +51,28 @@ infixr 1 ->:
 infixr 2  |:
 infixr 3  &:
 
+extractVariables :: PropFormula -> [Char]
+extractVariables p = removeDuplicates $ extractVariables' p []
+
+extractVariables' :: PropFormula -> [Char] -> [Char]
+extractVariables' (PropVariable ch) acc =   ch : acc
+extractVariables' (PropValue     v) acc =   acc
+extractVariables' (Not           p) acc =   extractVariables' p  acc
+extractVariables' (p1    :&     p2) acc =  (extractVariables' p1 acc) 
+                                        ++ (extractVariables' p2 acc)
+                                        ++  acc
+extractVariables' (p1    :|     p2) acc =  (extractVariables' p1 acc) 
+                                        ++ (extractVariables' p2 acc)
+                                        ++  acc
+extractVariables' (p1    :->    p2) acc =  (extractVariables' p1 acc) 
+                                        ++ (extractVariables' p2 acc)
+                                        ++  acc
+
+removeDuplicates :: Eq a => [a] -> [a]
+removeDuplicates = foldl (\seen x -> if x `elem` seen
+                                     then seen
+                                     else seen ++ [x]) []
+
 type PropVarMap = [(Char, LogicValue)]
 
 lookUpValue :: Char -> PropVarMap -> Maybe LogicValue
@@ -81,20 +84,22 @@ lookUpValue ch ((pv,v):tail) = if pv == ch
 retrieveValue :: PropFormula -> PropVarMap -> LogicValue
 retrieveValue (PropVariable ch) list
     | lookUpValue ch list == Nothing = error "No such propositional variable could be found"
-    | otherwise = fromJust $ lookUpValue ch list 
-retrieveValue (Not     p) list = anti $ retrieveValue p list
-retrieveValue (p1 :&  p2) list = (retrieveValue p1 list)  &: (retrieveValue p2 list)
-retrieveValue (p1 :|  p2) list = (retrieveValue p1 list)  |: (retrieveValue p2 list)
-retrieveValue (p1 :-> p2) list = (retrieveValue p1 list) ->: (retrieveValue p2 list)
+    | otherwise = fromJust $ lookUpValue ch list
+retrieveValue (PropValue     v) list = v
+retrieveValue (Not           p) list = anti $ retrieveValue p list
+retrieveValue (p1    :&     p2) list = (retrieveValue p1 list)  &: (retrieveValue p2 list)
+retrieveValue (p1    :|     p2) list = (retrieveValue p1 list)  |: (retrieveValue p2 list)
+retrieveValue (p1    :->    p2) list = (retrieveValue p1 list) ->: (retrieveValue p2 list)
 
 transform :: PropFormula -> PropVarMap -> String
 transform (PropVariable ch) list
     | lookUpValue ch list == Nothing = error "No such propositional variable could be found"
-    | otherwise = show . fromJust $ lookUpValue ch list
-transform (Not     p) list = "!(" ++ transform p  list ++ ")"
-transform (p1 :&  p2) list =  "(" ++ transform p1 list ++ " & "  ++ transform p2 list ++ ")"
-transform (p1 :|  p2) list =  "(" ++ transform p1 list ++ " v "  ++ transform p2 list ++ ")"
-transform (p1 :-> p2) list =  "(" ++ transform p1 list ++ " -> " ++ transform p2 list ++ ")"
+    | otherwise = show . PropValue . fromJust $ lookUpValue ch list
+transform (PropValue     v) list = if v == T then "1" else "0"
+transform (Not           p) list = "!(" ++ transform p  list ++ ")"
+transform (p1    :&     p2) list =  "(" ++ transform p1 list ++ " & "  ++ transform p2 list ++ ")"
+transform (p1    :|     p2) list =  "(" ++ transform p1 list ++ " v "  ++ transform p2 list ++ ")"
+transform (p1    :->    p2) list =  "(" ++ transform p1 list ++ " -> " ++ transform p2 list ++ ")"
 
 generateAllValues :: Int -> [[LogicValue]]
 generateAllValues 0 = [[]]
